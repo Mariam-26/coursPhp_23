e49165cc4cd7f9d62f908da40bacc890efda2edd
Mariam <82433410+Mariam-26@users.noreply.github.com>
2022-11-14 16:38:31 +0100

    testRoulette fonctionne pr nbrPair

--- testRoulette.php
+++ testRoulette.php
@@ -1,18 +1,31 @@
 <?php
 require_once("roulette.php");
 
-function testNbrAleatoire(): bool
-{
-  $result = nbrAleatoire();
-  if ($result === rand(0, 36)) {
-    return true;
-  } else {
-    return false;
-  }
+// function testNbrAleatoire(): bool
+// {
+//   $result = nbrAleatoire();
+//     /**
+//      * if ($result >= 0 && $result <= 36) {
+//      *  return true;
+//      * }
+//      */
+//     if($result < 0 || $result > 36){
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
+
+// if (testNbrAleatoire()) {
+//   echo "aleatoire ok";
+// } else {
+//   echo "aleatoire ko";
+// }
+
+function testNbrPair(){
+  $result = nbrPair(10);
+  $result1 = nbrPair(11);
+  return $result === true && $result1 === false;
 }
 
-if (testNbrAleatoire()) {
-  echo "aleatoire ok";
-} else {
-  echo "aleatoire ko";
-}
+echo testNbrPair() ? "\npair ok" : "\npair KO";
